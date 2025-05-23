1852cc222fe612f2ea3542c523c1074b963ce328
carlopcar4 <carloslopezcardenas01@gmail.com>
2025-05-23 10:02:48 +0200

    antes de lo chungo

--- backend/lib/crear_instancia.sh
+++ backend/lib/crear_instancia.sh
@@ -12,16 +12,25 @@ id=$1
     puerto2=$(jq -r .puerto_proc_part datos.json)
     logo=$(jq -r .logo_url datos.json)
     banner=$(jq -r .banner_url datos.json)
-    colab=$(jq -r .collaborations datos.json)
-    servicios=$(jq -r .services datos.json)
+    colab=$(jq -r '.collaborations | join(",")' datos.json)
+    servicios=$(jq -r '.services | join(",")' datos.json)
     echo "Avanzando un poco más"
 
-crear_instancia() {
-    if [ -d /home/carlos/TFG/"decidim_$nombre" ]
-    then
-        echo "El directorio para la instancia $nombre ya existe"
-    else
-        cp -r /home/carlos/TFG/decidim_base/prueba/ /home/carlos/TFG/"decidim_$nombre"
-    fi
-}
-crear_instancia
+# ESTO ES PARA PRUEBA
+    rm -rf /home/carlos/TFG/"decidim_$nombre"
+
+# CREACIÓN DE LA INSTANCIA
+    cp -r /home/carlos/TFG/decidim_base/prueba/ /home/carlos/TFG/"decidim_$nombre"
+
+# CREACIÓN DE .ENV
+    cd /home/carlos/TFG/"decidim_$nombre"/
+    touch .env
+    echo "NOMBRE=$nombre" > .env
+    echo "PUERTO1=$puerto1" >> .env
+    echo "LOGO=$logo" >> .env
+    echo "BANNER=$banner" >> .env
+    echo "COLAB=$colab" >> .env
+    echo "SERVICIOS=$servicios" >> .env
+
+    docker-compose --env-file .env up -d
+
