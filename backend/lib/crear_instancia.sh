149d4226acca10fa67086a978aa7ba391274b6db
carlopcar4 <carloslopezcardenas01@gmail.com>
2025-05-26 17:25:34 +0200

    liado

--- backend/lib/crear_instancia.sh
+++ backend/lib/crear_instancia.sh
@@ -25,23 +25,116 @@ function crear_carpeta() {
     cd "decidim_$nombre"
     nombre_docker="${nombre//-/_}"
     touch .env
-    echo "NOMBRE=$nombre" > .env
+    echo "nombre=$nombre" > .env
     echo "nombre_docker=${nombre//-/_}" >> .env
-    echo "PUERTO1=$puerto1" >> .env
-    echo "LOGO=$logo" >> .env
-    echo "BANNER=$banner" >> .env
-    echo "COLAB=$colab" >> .env
-    echo "SERVICIOS=$servicios" >> .env
-    echo "DATABASE_URL=postgres://postgres:postgres@pg:5432/decidim_${nombre_docker}" >> .env
+    echo "puerto1=$puerto1" >> .env
+    echo "logo=$logo" >> .env
+    echo "banner=$banner" >> .env
+    echo "colab=$colab" >> .env
+    echo "servicios=$servicios" >> .env
+    echo "database_url=postgres://postgres:postgres@pg:5432/decidim_${nombre_docker}" >> .env
 }
 
 function crear_docker() {
+    docker exec -it decidim_${nombre_docker}_pg_1 psql -U postgres -c "DROP DATABASE IF EXISTS decidim_${nombre_docker}"
     docker-compose --env-file .env down --remove-orphans
     docker-compose --env-file .env up -d
     docker exec -it decidim_${nombre_docker}_app_1 bundle exec rails db:create
     docker exec -it decidim_${nombre_docker}_app_1 bundle exec rails db:migrate
 }
 
+function crear_organizacion() {
+
+    docker exec -i decidim_${nombre_docker}_app_1 bundle exec rails runner - <<EOF
+organizacion = Decidim::Organization.create!(
+    name: "$nombre" ,
+    host: "${nombre}.localhost:$puerto1",
+    available_locales: [:es],
+    default_locale: :es,
+    reference_prefix: "${nombre_docker:0:3}"
+)
+puts "Organización creada"
+EOF
+
+
+    host_ip=$(docker exec -i tfg_backend_1 ip route | awk '/default/ { print $3 }')
+    logo_url_sustituido=$(echo "$logo" | sed "s|localhost:4000|$host_ip:4000|g")
+    docker exec -i decidim_${nombre_docker}_app_1 curl -L -o /tmp/logo.png "$logo_url_sustituido"
+
+    docker exec -i decidim_${nombre_docker}_app_1 bundle exec rails runner - <<EOF
+org = Decidim::Organization.last
+org.logo.attach(
+    io: File.open("/tmp/logo.png"),
+    filename: "logo.png",
+    content_type: "image/png"
+)
+org.save!
+puts "Logo adjuntado"
+EOF
+
+    banner_url_sustituido=$(echo "$banner" | sed "s|localhost:4000|$host_ip:4000|g")
+    docker exec -i decidim_${nombre_docker}_app_1 curl -L -o /tmp/banner.jpg "$banner_url_sustituido"
+
+    docker exec -i decidim_${nombre_docker}_app_1 bundle exec rails runner - <<EOF
+org = Decidim::Organization.last
+org.highlighted_content_banner_image.attach(
+    io: File.open("/tmp/banner.jpg"),
+    filename: "lobannergo.jpg",
+    content_type: "image/jpg"
+)
+org.save!
+puts "banner adjuntado"
+EOF
+
+
+    docker exec -i decidim_${nombre_docker}_app_1 bundle exec rails runner - <<EOF
+admin = Decidim::System::Admin.new(
+    email: "admin@admin.org",
+    password: "contraseña123",
+    password_confirmation: "contraseña123"
+)
+admin.save!
+puts "Usuario creado"
+EOF
+
+
+
+    docker exec -i decidim_${nombre_docker}_app_1 bundle exec rails runner - <<EOF
+org=Decidim::Organization.last
+process = Decidim::ParticipatoryProcess.create!(
+    organization: org, 
+    title: { es: "hola" }, 
+    subtitle: { es: "hola" }, 
+    description: { es: "hoa" }, 
+    slug: "sde", 
+    short_description: { es: "hoa" },
+    private_space: false
+)
+puts "Proceso creado"
+EOF
+
+    docker exec -i decidim_${nombre_docker}_app_1 bundle exec rails runner - <<EOF
+org=Decidim::Organization.last
+pag = Decidim::StaticPage.create!(
+    organization: org, 
+    title: { es: "Inicio" }, 
+    slug: "home", 
+    content: { es: "<h1>Bienvenido a la plataforma</h1><p>Esta es la portada.</p>" },
+
+
+)
+
+puts "Proceso creado"
+EOF
+
+
+
+}
+
+
+
+
 datos $id 
 crear_carpeta
 crear_docker
+crear_organizacion
--- flask/app.py
+++ flask/app.py
@@ -7,14 +7,9 @@ CORS(app)
 
 @app.route('/deploy/<id>', methods=['POST'])
 def deploy(id):
-    command = f"require '/app/lib/deploy_instance.rb'; DeployInstance.create({id})"
-    docker = ['docker', 'exec', 'tfg_decidim_1', 'rails', 'runner', command]
-    try:
-        result = subprocess.run(docker, capture_output=True, text=True, check=True)
-        return f"Despliegue ejecutado: {result.stdout}", 200
-    except subprocess.CalledProcessError as e:
-        return f"Error en el despliegue:\nSTDOUT:\n{e.stdout}\nSTDERR:\n{e.stderr}", 500
-
+    subprocess.call(["/bin/bash", "/home/carlos/TFG/backend/lib/crear_instancia.sh", str(id)])
+    return {"status": "ok"}, 200
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4001)
